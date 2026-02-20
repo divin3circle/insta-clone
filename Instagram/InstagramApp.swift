ef1fd56106d4c25d090c0b86a318f0e0fc224540
divin3circle <sylus.abel@icloud.com>
2026-02-20 19:31:27 +0300

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Sylus Abel on 20/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
